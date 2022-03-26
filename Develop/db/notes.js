0c7d4a6503a0e6ba3200ce3181d46693ff757270
Calvin Turner <turnerjrcalvin@gmail.com>
2022-03-26 19:03:26 -0400

    store notes

--- Develop/db/notes.js
+++ Develop/db/notes.js
@@ -3,4 +3,23 @@ const fs = require('fs');
 const util= require('util');
 const readFileAsync =util.promisify(fs.readFile);
 
-class Notes 
+class Notes {
+    readFile(){
+        return readFileAsync ('db/db.json', 'utf8');
+    }
+    writefile(note){
+        return fs.writeFileSync('db/db.json', JSON.stringify(note));
+    }
+    getNotes() {
+        return this.readFile().then(rawNotes => {
+            let notesArray;
+            try{
+                notesArray=[].concat(JSON.parse(rawNotes))
+            } catch (error) {
+                notesArray=[];
+            }
+            return notesArray;
+         });
+    }
+}
+module.exports=new Notes();
