3d5dc2fa37711a8ab0b8e485138f1793b08d48d5
Calvin Turner <turnerjrcalvin@gmail.com>
2022-03-29 00:00:35 -0400

    establish end points

--- Develop/server.js
+++ Develop/server.js
@@ -5,3 +5,22 @@ const db = require('./db/db.json') ;
 const { v4: uuidv4} = require('uuid');
 
 const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () =>
+console.log(`Note app listening at http://localhost:${port}`)
+);
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(_dirname, 'public/notes.html'));
+});
+
+app.get('*', (req,res) =>
+res.sendFile(path.join(_dirname, 'public/index.html'))
+);
+
+app
