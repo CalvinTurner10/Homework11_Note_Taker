865356fbe74ef8548cc66e69b3989ea560f978a7
Calvin Turner <turnerjrcalvin@gmail.com>
2022-03-29 21:21:46 -0400

    notes

--- path/api.js
+++
@@ -1,7 +0,0 @@
-const path=require('express').Path();
-const Notes=require('../db/notes');
-const fs = require("fs");
-const {v4: uuidv4} = require('uuid');
-const {response } = require('express');
-const {ok} = require('assert');
-const notes = require('../db/notes');
